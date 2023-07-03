197ea1b78970c75739f42faf6fcdce2f6256b78f
Hugo <hugoo.ribeiro03@gmail.com>
2023-07-03 11:11:53 -0300

    atualizando usuario e imagem

--- src/User.js
+++ src/User.js
@@ -1,11 +1,28 @@
+import {useState} from "react";
+
 export default function User(){
+
+    let [novaImagem, setImagem] = useState("");
+
+    function trocarImagem(){
+        const imagem = prompt("Digite a nova imagem");
+        setImagem(imagem);
+    }
+
+    let [nomeUsuario, setNomeUsuario] = useState("");
+
+    function perguntarNome(){
+        const nome = prompt("Digite seu nome");
+        setNomeUsuario(nome);
+    }
+
     return (
         <div class="usuario">
-            <img data-test="profile-image" src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+            <img data-test="profile-image" onClick={trocarImagem} src={(!novaImagem) ? "assets/img/catanacomics.svg" : {novaImagem}} alt="imagem de perfil"/>
             <div class="texto">
                 <span>
-                <strong data-test="name" >catanacomics</strong>
-                <ion-icon name="pencil" data-test="edit-name"></ion-icon>
+                <strong data-test="name" >{(!nomeUsuario) ? "catanacomics" : `${nomeUsuario}`}</strong>
+                <ion-icon name="pencil" data-test="edit-name" onClick={perguntarNome} ></ion-icon>
                 </span>
             </div>
         </div>
