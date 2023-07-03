2e9f1e81c282298cdbbcede6438aa6d5295c873a
Hugo <hugoo.ribeiro03@gmail.com>
2023-07-03 14:16:50 -0300

    versao numero de curtidas

--- src/Post.js
+++ src/Post.js
@@ -7,14 +7,29 @@ export default function Post(){
         {imagem:"assets/img/9gag.svg",titulo:"9gag",imagemPost:"assets/img/gato-telefone.svg"}
     ];
 
-    let [cor, setCor] = useState("");
+    let [corVermelho, setCorVermelho] = useState("");
 
-    function trocarCor(){
-        if ( cor === ""){
-            
-            setCor("vermelho");
+    let [curtidas, setCurtidas] = useState("101.523");
+
+    function trocarCorCurtida(){
+        
+        if ( corVermelho === ""){
+            setCurtidas("101.524");            
+            setCorVermelho("vermelho");
         }else{
-            setCor("");
+            setCurtidas("101.523");
+            setCorVermelho("");
+        }
+    }
+
+    let [corAzul, setCorAzul] = useState("");
+
+    function trocarCorSalvar(){
+        
+        if ( corAzul === ""){            
+            setCorAzul("azul");
+        }else{
+            setCorAzul("");
         }
     }
     return(
@@ -32,25 +47,25 @@ export default function Post(){
                     </div>
 
                     <div class="conteudo">
-                        <img src={item.imagemPost} alt="gato-telefone" data-test="post-image" onClick={trocarCor}/>
+                        <img src={item.imagemPost} alt="gato-telefone" data-test="post-image" onClick={trocarCorCurtida}/>
                     </div>
 
                     <div class="fundo">
                         <div class="acoes">
                             <div>
-                                <ion-icon name="heart-outline" data-test="like-post" class={cor} onClick={trocarCor} ></ion-icon>
+                                <ion-icon name="heart-outline" data-test="like-post" class={corVermelho} onClick={trocarCorCurtida} ></ion-icon>
                                 <ion-icon name="chatbubble-outline"></ion-icon>
                                 <ion-icon name="paper-plane-outline"></ion-icon>
                             </div>
                             <div>
-                                <ion-icon name="bookmark-outline" data-test="save-post"></ion-icon>
+                                <ion-icon name="bookmark-outline" data-test="save-post" class={corAzul} onClick={trocarCorSalvar}></ion-icon>
                             </div>
                         </div>
 
                         <div class="curtidas">
                             <img src="assets/img/respondeai.svg" alt="respondeai"/>
                             <div class="texto">
-                                Curtido por <strong>respondeai</strong> e <strong data-test="likes-number">outras 101.523 pessoas</strong>
+                                Curtido por <strong>respondeai</strong> e <strong data-test="likes-number">outras {curtidas} pessoas</strong>
                             </div>
                         </div>
                     </div>
