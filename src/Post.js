1c85c2bbe80c1d6e78dea2bdac8fd1311699d4ce
Hugo <hugoo.ribeiro03@gmail.com>
2023-07-03 08:49:17 -0300

    versao data-test

--- src/Post.js
+++ src/Post.js
@@ -8,7 +8,7 @@ export default function Post(){
     return(
         <div class="posts">
             {array.map((item) =>
-                <div class="post">
+                <div class="post" data-test="post">
                     <div class="topo">
                         <div class="usuario">
                             <img src={item.imagem} alt={item.titulo}/>
@@ -20,25 +20,25 @@ export default function Post(){
                     </div>
 
                     <div class="conteudo">
-                        <img src={item.imagemPost} alt="gato-telefone"/>
+                        <img src={item.imagemPost} alt="gato-telefone" data-test="post-image"/>
                     </div>
 
                     <div class="fundo">
                         <div class="acoes">
                             <div>
-                                <ion-icon name="heart-outline"></ion-icon>
+                                <ion-icon name="heart-outline" data-test="like-post"></ion-icon>
                                 <ion-icon name="chatbubble-outline"></ion-icon>
                                 <ion-icon name="paper-plane-outline"></ion-icon>
                             </div>
                             <div>
-                                <ion-icon name="bookmark-outline"></ion-icon>
+                                <ion-icon name="bookmark-outline" data-test="save-post"></ion-icon>
                             </div>
                         </div>
 
                         <div class="curtidas">
                             <img src="assets/img/respondeai.svg" alt="respondeai"/>
                             <div class="texto">
-                                Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+                                Curtido por <strong>respondeai</strong> e <strong data-test="likes-number">outras 101.523 pessoas</strong>
                             </div>
                         </div>
                     </div>
--- src/User.js
+++ src/User.js
@@ -1,11 +1,11 @@
 export default function User(){
     return (
         <div class="usuario">
-            <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+            <img data-test="profile-image" src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
             <div class="texto">
                 <span>
-                <strong>catanacomics</strong>
-                <ion-icon name="pencil"></ion-icon>
+                <strong data-test="name" >catanacomics</strong>
+                <ion-icon name="pencil" data-test="edit-name"></ion-icon>
                 </span>
             </div>
         </div>
